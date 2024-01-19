eeff33eff60e60e692637db218431d7471de22ee
차민재 <zoro317@naver.com>
2024-01-19 09:57:22 +0900

    [FE] chore : 프론트엔드 레이아웃 설정 및 필요 라이브러리 설치

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import StartView from './views/StartView';
+import FoodMapView from './views/FoodMapView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/* 로그인 했을 때는 이 레이아웃으로 */}
+      <Route path="/" element={<FoodMapView />} />
+
+      {/* 로그인 하지않았을 때는 이 레이아웃으로 */}
+      <Route path="/start" element={<StartView />} />
+    </Routes>
   );
 }
 
---
+++ FE/src/components/start/StartHeader.jsx
@@ -0,0 +1,5 @@
+function StartHeader() {
+  return <div>로그인 안 했을 때 헤더</div>;
+}
+
+export default StartHeader;
