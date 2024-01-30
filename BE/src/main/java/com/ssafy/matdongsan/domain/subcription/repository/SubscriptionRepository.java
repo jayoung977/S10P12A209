220db05824f998e07c1025dd9da81884093ebf3a
김준엽 <top1506@naver.com>
2024-01-30 12:27:53 +0900

    [BE] Fix : Cors 규칙 변경

--- BE/src/main/java/com/ssafy/matdongsan/domain/subcription/repository/SubscriptionRepository.java
+++ BE/src/main/java/com/ssafy/matdongsan/domain/subcription/repository/SubscriptionRepository.java
@@ -1,8 +1,9 @@
-//package com.ssafy.matdongsan.domain.subcription.repository;
-//
-//import com.ssafy.matdongsan.domain.subcription.model.Subscription;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface SubscriptionRepository extends JpaRepository<Subscription, Short> {
-//
-//}
+package com.ssafy.matdongsan.domain.subcription.repository;
+
+import com.ssafy.matdongsan.domain.subcription.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Short> {
+}
