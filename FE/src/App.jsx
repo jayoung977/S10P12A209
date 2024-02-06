1e4d03bd7c7857fddbeb4912a5f65650bcaff2f3
이다은 <dlekdms7234@naver.com>
2024-02-07 00:15:00 +0900

    [FE] fix: 카카오 로그인 수정

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -4,7 +4,8 @@ import FoodMapView from './views/FoodMapView';
 import SignupView from './views/SignupView';
 // import Redirect from './components/accounts/Redirect';
 import EditProfileView from './views/EditProfileView';
-import SocialLogin from './components/accounts/SocialLogin';
+// import SocialLogin from './components/accounts/SocialLogin';
+import KakaoLogin from './components/accounts/KakaoLogin';
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
       <Route path="/main/*" element={<FoodMapView />} />
       {/* 로그인 하지않았을 때는 이 레이아웃으로 */}
       <Route path="*" element={<StartView />} />
-      <Route path="/sociallogin" element={<SocialLogin />} />
-      <Route path="/signup" element={<SignupView />} />
+      <Route path="/oauth2/kakao" element={<KakaoLogin />} />
+      <Route path="/oauth2/kakao/signup" element={<SignupView />} />
       <Route path="/editprofile" element={<EditProfileView />} />
     </Routes>
   );
