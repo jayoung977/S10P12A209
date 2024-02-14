0eed15a3ae345cbff5126277188ff5a53a7c6796
차민재 <zoro317@naver.com>
2024-02-14 15:10:33 +0900

    [FE] Refactor/maplocation 1번 맛집 위치로 지도 설정

--- FE/src/components/foodmap/FoodMap.jsx
+++ FE/src/components/foodmap/FoodMap.jsx
@@ -31,22 +31,6 @@ function FoodMap() {
         // eslint-disable-next-line max-depth
         if (localStorage.getItem('ACCESS_TOKEN')) {
           console.log('로그인한 지도');
-          // 네이버 지도 옵션 선택
-          const mapOptions = {
-            // 지도의 초기 중심 좌표
-            center: new naver.maps.LatLng(
-              currentMyLocation.lat,
-              currentMyLocation.lng
-            ),
-            logoControl: false, // 네이버 로고 표시 X
-            mapDataControl: false, // 지도 데이터 저작권 컨트롤 표시 X
-            scaleControl: true, // 지도 축척 컨트롤의 표시 여부
-            tileDuration: 200, // 지도 타일을 전환할 때 페이드 인 효과의 지속 시간(밀리초)
-            zoom: 16, // 지도의 초기 줌 레벨
-            zoomControl: true, // 줌 컨트롤 표시
-            zoomControlOptions: { position: 9 }, // 줌 컨트롤 우하단에 배치
-          };
-          mapRef.current = new naver.maps.Map('map', mapOptions);
 
           // 사용자 정보 받아오기
           const fetchUserData = async () => {
@@ -90,6 +74,39 @@ function FoodMap() {
           fetchUserData()
             .then((userIds) => fetchRestaurantData(userIds))
             .then((mapDatas) => {
+              let centerLat = 0;
+              let centerLng = 0;
+
+              for (let i = 0; i < mapDatas.userIds.length; i += 1) {
+                for (
+                  let j = 0;
+                  j < mapDatas.restaurantLists[i].length;
+                  j += 1
+                ) {
+                  centerLat =
+                    mapDatas.restaurantLists[i][j].mapy / tenPowSeven;
+                  centerLng =
+                    mapDatas.restaurantLists[i][j].mapx / tenPowSeven;
+                }
+                if (centerLat !== 0 && centerLng !== 0) {
+                  break;
+                }
+              }
+
+              // 네이버 지도 옵션 선택
+              const mapOptions = {
+                // 지도의 초기 중심 좌표
+                center: new naver.maps.LatLng(centerLat, centerLng),
+                logoControl: false, // 네이버 로고 표시 X
+                mapDataControl: false, // 지도 데이터 저작권 컨트롤 표시 X
+                scaleControl: true, // 지도 축척 컨트롤의 표시 여부
+                tileDuration: 200, // 지도 타일을 전환할 때 페이드 인 효과의 지속 시간(밀리초)
+                zoom: 16, // 지도의 초기 줌 레벨
+                zoomControl: true, // 줌 컨트롤 표시
+                zoomControlOptions: { position: 9 }, // 줌 컨트롤 우하단에 배치
+              };
+              mapRef.current = new naver.maps.Map('map', mapOptions);
+
               console.log('지도에 필요한 모든 데이터', mapDatas);
               const markers = [];
               const infoWindows = [];
